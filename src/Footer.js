dce9913bf2732c238414125e735b0c7b763b9783
Randy Fox <rfox@keyholesoftware.com>
2023-07-18 14:49:01 -0500

    hit counter

--- src/Footer.js
+++ src/Footer.js
@@ -6,6 +6,8 @@ const Footer = () => {
         <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <div align='center'><a href='https://www.free-website-hit-counter.com'><img src='https://www.free-website-hit-counter.com/c.php?d=9&id=153098&s=18' border='0' alt='Free Website Hit Counter'/></a><br / ><small><a href='https://www.free-website-hit-counter.com' title="Free Website Hit Counter">Free website hit counter</a></small></div>
+
         </>
      );
 }
